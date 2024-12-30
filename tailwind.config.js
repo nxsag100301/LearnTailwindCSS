38045882b0295cbc323d643ca93536bef8c8717a
Xuan Sang <nguyenxuansang10032001@gmail.com>
2024-12-30 22:06:47 +0700

    responesiveHeader

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       },
       colors: {
         "light-coffe": "#C89F94"
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.5s ease-in-out',
       }
     },
   },
