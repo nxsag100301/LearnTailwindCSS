8a67b67968e713a20336d9f38b321cd2a941f0c8
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-01-13 18:29:27 +0700

    doneStory

--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
-const topMenu = document.getElementById("nxs-top-menu-responsive");
-const topMenuToggleIcon = document.getElementById("nxs-top-menu-toggle-icon");
+const topMenu = document.getElementById('nxs-top-menu-responsive')
+const topMenuToggleIcon = document.getElementById('nxs-top-menu-toggle-icon')
 
-document.addEventListener("click", function (event) {
-    if (topMenuToggleIcon.contains(event.target)) {
-        topMenu.classList.toggle("hidden");
-        topMenu.classList.toggle("nxs-top-menu-mobile");
-    } else if (topMenu.classList.contains("nxs-top-menu-mobile")) {
-        topMenu.classList.add("hidden");
-        topMenu.classList.remove("nxs-top-menu-mobile");
-    }
-});
+document.addEventListener('click', function (event) {
+  if (topMenuToggleIcon.contains(event.target)) {
+    topMenu.classList.toggle('hidden')
+    topMenu.classList.toggle('nxs-top-menu-mobile')
+  } else if (topMenu.classList.contains('nxs-top-menu-mobile')) {
+    topMenu.classList.add('hidden')
+    topMenu.classList.remove('nxs-top-menu-mobile')
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
-        Karla: ['Karla', 'serif']
+        Karla: ['Karla', 'serif'],
       },
       colors: {
-        "light-coffe": "#C89F94"
+        'custom-blue': '#050827',
+        'coffee': {
+          50: '#E8D6D0',
+          200: '#C89F94',
+          400: '#A25F4B',
+          600: '#744838',
+        },
       },
       keyframes: {
         slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        }
+          '0%': { transform: 'translateY(-20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
       },
       animation: {
-        slideDown: 'slideDown 0.5s ease-in-out',
-      }
+        slideDown: 'slideDown 0.7s ease-in-out',
+      },
     },
   },
   plugins: [],
-}
-
+};
